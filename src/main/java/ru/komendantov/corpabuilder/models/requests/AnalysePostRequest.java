794cb1675fd237aea243ebaf304cf8d2f538ae46
Anatoliy <anatoly.komendantov@gmail.com>
2020-05-25 01:32:01 +0400

    minor fix

--- src/main/java/ru/komendantov/corpabuilder/models/requests/AnalysePostRequest.java
+++ src/main/java/ru/komendantov/corpabuilder/models/requests/AnalysePostRequest.java
@@ -1,9 +1,11 @@
 package ru.komendantov.corpabuilder.models.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
 @Data
+@AllArgsConstructor
 public class AnalysePostRequest {
     @NonNull
     private String text;
