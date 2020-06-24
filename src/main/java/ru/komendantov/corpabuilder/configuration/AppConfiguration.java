09b0e25a14f714fa321de2faa6a5a3d86357b9f6
Anatoliy Komendantov <anatoly.komendantov@gmail.com>
2020-06-24 14:20:03 +0400

    encoding

--- src/main/java/ru/komendantov/corpabuilder/configuration/AppConfiguration.java
+++ src/main/java/ru/komendantov/corpabuilder/configuration/AppConfiguration.java
@@ -1,7 +1,16 @@
 package ru.komendantov.corpabuilder.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 public class AppConfiguration {
+    @Bean
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+    }
 }
