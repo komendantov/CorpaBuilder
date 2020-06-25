81c584df6d1949be64775c81c780a4e8190ffe88
Anatoliy <anatoly.komendantov@gmail.com>
2020-06-26 00:35:00 +0400

    deployment fixes

--- src/main/java/ru/komendantov/corpabuilder/services/MystemService.java
+++ src/main/java/ru/komendantov/corpabuilder/services/MystemService.java
@@ -3,6 +3,7 @@ package ru.komendantov.corpabuilder.services;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.net.URL;
 
 @Service
 public class MystemService {
@@ -11,12 +12,13 @@ public class MystemService {
         boolean isWindows = System.getProperty("os.name")
                 .toLowerCase().startsWith("windows");
         ProcessBuilder builder = new ProcessBuilder();
+        String mystemPath = System.getProperty("mystemPath");
         String command;
-        if (isWindows) {
-            command = "./mystem.exe -cisd --eng-gr --generate-all --format json " + targetFile.getCanonicalPath();
-        } else {
-            command = "./mystem -cisd --eng-gr --generate-all --format json " + targetFile.getCanonicalPath();
-        }
+      //  if (isWindows) {
+            command = mystemPath + " -cisd --eng-gr --generate-all --format json " + targetFile.getCanonicalPath();
+      //  } else {
+      //      command = mystemPath + " -cisd --eng-gr --generate-all --format json " + targetFile.getCanonicalPath();
+      //  }
         Process process = Runtime.getRuntime().exec(command);
         builder.directory(new File(System.getProperty("user.dir")));
         return process;
